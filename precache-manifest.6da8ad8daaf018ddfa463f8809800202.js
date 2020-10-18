3ab6084f4408af390371a892bba67ee057ef165b
VIKI-09 <46499119+VIKI-09@users.noreply.github.com>
2020-10-19 02:11:19 +0300

    Updates

---
+++ precache-manifest.6da8ad8daaf018ddfa463f8809800202.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd74ce3f939044140558126c0443d2f1",
+    "url": "/pockemon-test-task/index.html"
+  },
+  {
+    "revision": "8f76bbb316f746d7b621",
+    "url": "/pockemon-test-task/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "99bbe9a6b889f0185639",
+    "url": "/pockemon-test-task/static/js/2.fc004ee0.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/pockemon-test-task/static/js/2.fc004ee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f76bbb316f746d7b621",
+    "url": "/pockemon-test-task/static/js/main.1cb09d8b.chunk.js"
+  },
+  {
+    "revision": "64fc9b3f3cf119e4ae92",
+    "url": "/pockemon-test-task/static/js/runtime-main.92e98f10.js"
+  }
+]);
